d4633f15be09263ab9eaaa442017f608d570062b
Young <navskh@naver.com>
2022-09-21 15:05:48 +0900

    조아써

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,22 @@
 <template>
   <div class="max-w-[1500px] m-auto">
-    <router-view />
+    <Header />
+    <div class="flex">
+      <Sidebar />
+      <router-view />
+    </div>
+    <Footer />
   </div>
 </template>
 
 <script>
+import Header from "./components/Header.vue";
+import Sidebar from "./components/sidebar/Sidebar.vue";
+import Footer from "./components/Footer.vue";
+
 export default {
   name: "App",
+  components: { Header, Sidebar, Footer },
 };
 </script>
 
--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,17 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Main from "../views/Main";
+import PostDetailVue from "@/views/PostDetail.vue";
 
 const routes = [
   {
     path: "/:divOne?/:divTwo?/:divThree?/:postNo?",
     name: "main",
     component: Main,
+  },
+  {
+    path: "/1",
+    name: "detail",
+    component: PostDetailVue,
   },
 ];
 
