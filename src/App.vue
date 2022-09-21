144f48b626a48ae7faa93ba452db84b600182f75
wattk <watt.kwon@gmail.com>
2022-09-21 14:36:23 +0900

    refactor: reorganize code

--- src/App.vue
+++ src/App.vue
@@ -2,22 +2,11 @@
   <div class="max-w-[1500px] m-auto">
     <router-view />
   </div>
-  <!-- <Header />
-  <div class="flex">
-    <Sidebar />
-    <Article />
-  </div>
-  <Footer /> -->
 </template>
 
 <script>
-// import Header from "./components/Header.vue";
-// import Sidebar from "./components/Sidebar.vue";
-// import Footer from "./components/Footer.vue";
-// import Article from "./components/Article.vue";
 export default {
   name: "App",
-  // components: { Header, Sidebar, Footer, Article },
 };
 </script>
 
