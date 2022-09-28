a3b60d8002e67a1e5a4fcbc287a85ce10c5e7a46
Young <navskh@naver.com>
2022-09-29 01:50:57 +0900

    오케이 고고고고!

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import './assets/tailwind.css';
+import 'remixicon/fonts/remixicon.css';
 import router from './router';
+import './assets/@tailwind/typography.css';
 
 createApp(App).use(router).mount('#app');
