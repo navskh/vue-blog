2187204c51aba54223c7666a8485269578a77cc7
jhk88 <deftones88@naver.com>
2022-11-10 11:37:35 +0900

    style: conten width fixed

--- src/components/PostInfoBar.vue
+++ src/components/PostInfoBar.vue
@@ -1,42 +1,42 @@
 <template>
-  <div class="DETAIL w-full h-full pb-1 overflow-y-scroll">
+<div class="DETAIL max-w-[calc(1500px-18rem)] h-full pb-1 overflow-y-scroll">
     <div class="flex justify-between">
-      <div class="text-sm breadcrumbs text-primary-focus flex-row">
-				<ul>
-					<li>
-						<a>{{ props.upper }}</a>
-					</li>
-					<li>
-						<a>{{ props.sub }}</a>
-					</li>
-					<li>
-						<a>{{ props.detail }}</a>
-					</li>
-				</ul>
-			</div>
+		<div class="text-sm breadcrumbs text-primary-focus flex-row">
+			<ul>
+				<li>
+					<a>{{ props.upper }}</a>
+				</li>
+				<li>
+					<a>{{ props.sub }}</a>
+				</li>
+				<li>
+					<a>{{ props.detail }}</a>
+				</li>
+			</ul>
 		</div>
-		<h1 class="font-bold text-3xl py-3">{{ props.title }}</h1>
-		<div class="flex justify-between items-center border-b-2 pb-2">
-			<div class="text-sm">
-				<span class="mr-1">{{ formatDate(props.createdAt) }}</span>
-				<span class="text-xs">
-					마지막으로 수정한 사람 : {{ props.author }}</span
-				>
-			</div>
-			<div>
-				<button class="btn btn-ghost btn-sm">
-					<ShareIcon class="w-5 h-5" />
-				</button>
-				<button class="btn btn-ghost btn-sm" @click="goEdit()">
-					<PencilIcon class="w-5 h-5" />
-				</button>
-				<button class="btn btn-ghost btn-sm" @click="confirmDelete()">
-					<TrashIcon class="w-5 h-5" />
-				</button>
-			</div>
-		</div>
-		<div v-html="props.content" class="ProseMirror prose-lg h-max my-5"></div>
 	</div>
+	<h1 class="font-bold text-3xl py-3">{{ props.title }}</h1>
+	<div class="flex justify-between items-center border-b-2 pb-2">
+		<div class="text-sm">
+			<span class="mr-1">{{ formatDate(props.createdAt) }}</span>
+			<span class="text-xs">
+				마지막으로 수정한 사람 : {{ props.author }}</span
+			>
+		</div>
+		<div>
+			<button class="btn btn-ghost btn-sm">
+				<ShareIcon class="w-5 h-5" />
+			</button>
+			<button class="btn btn-ghost btn-sm" @click="goEdit()">
+				<PencilIcon class="w-5 h-5" />
+			</button>
+			<button class="btn btn-ghost btn-sm" @click="confirmDelete()">
+				<TrashIcon class="w-5 h-5" />
+			</button>
+		</div>
+	</div>
+	<div v-html="props.content" class="ProseMirror prose-lg h-max my-5"></div>
+</div>
 </template>
 <script setup>
 import { ShareIcon, PencilIcon, TrashIcon } from '@heroicons/vue/24/solid';
@@ -110,4 +110,5 @@ const doDelete = async () => {
 .DETAIL::-webkit-scrollbar-track {
   background-color: rgba(255, 255, 255, 0);
 }
+
 </style>
