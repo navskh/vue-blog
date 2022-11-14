435e795774be979e0b3d27c71d8ef83e37dfd592
wattk <watt.kwon@gmail.com>
2022-11-14 15:03:05 +0900

    fix: content text color css 수정

--- src/components/PostInfoBar.vue
+++ src/components/PostInfoBar.vue
@@ -1,49 +1,51 @@
 <template>
-<div class="DETAIL max-w-[calc(1500px-18rem)] h-full pb-1 overflow-y-scroll">
+  <div class="DETAIL max-w-[calc(1500px-18rem)] h-full pb-1 overflow-y-scroll">
     <div class="flex justify-between">
-		<div class="text-sm breadcrumbs text-primary-focus flex-row">
-			<ul>
-				<li>
-					<a>{{ props.upper }}</a>
-				</li>
-				<li>
-					<a>{{ props.sub }}</a>
-				</li>
-				<li>
-					<a>{{ props.detail }}</a>
-				</li>
-			</ul>
-		</div>
-	</div>
-	<h1 class="font-bold text-3xl py-3">{{ props.title }}</h1>
-	<div class="flex justify-between items-center border-b-2 pb-2">
-		<div class="text-sm">
-			<span class="mr-1">{{ formatDate(props.createdAt) }}</span>
-			<span class="text-xs">
-				마지막으로 수정한 사람 : {{ props.author }}</span
-			>
-		</div>
-		<div>
-			<button class="btn btn-ghost btn-sm">
-				<ShareIcon class="w-5 h-5" />
-			</button>
-			<button class="btn btn-ghost btn-sm" @click="goEdit()">
-				<PencilIcon class="w-5 h-5" />
-			</button>
-			<button class="btn btn-ghost btn-sm" @click="confirmDelete()">
-				<TrashIcon class="w-5 h-5" />
-			</button>
-		</div>
-	</div>
-	<div class="ProseMirror prose h-max my-5 "><div v-html="props.content" class="text-base-content"></div></div>
-</div>
+      <div class="text-sm breadcrumbs text-primary-focus flex-row">
+        <ul>
+          <li>
+            <a>{{ props.upper }}</a>
+          </li>
+          <li>
+            <a>{{ props.sub }}</a>
+          </li>
+          <li>
+            <a>{{ props.detail }}</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+    <h1 class="font-bold text-3xl py-3">{{ props.title }}</h1>
+    <div class="flex justify-between items-center border-b-2 pb-2">
+      <div class="text-sm">
+        <span class="mr-1">{{ formatDate(props.createdAt) }}</span>
+        <span class="text-xs">
+          마지막으로 수정한 사람 : {{ props.author }}</span
+        >
+      </div>
+      <div>
+        <button class="btn btn-ghost btn-sm">
+          <ShareIcon class="w-5 h-5" />
+        </button>
+        <button class="btn btn-ghost btn-sm" @click="goEdit()">
+          <PencilIcon class="w-5 h-5" />
+        </button>
+        <button class="btn btn-ghost btn-sm" @click="confirmDelete()">
+          <TrashIcon class="w-5 h-5" />
+        </button>
+      </div>
+    </div>
+    <div class="ProseMirror prose h-max my-5">
+      <div v-html="props.content"></div>
+    </div>
+  </div>
 </template>
 <script setup>
-import { ShareIcon, PencilIcon, TrashIcon } from '@heroicons/vue/24/solid';
-import { useRouter, useRoute } from 'vue-router';
-import { defineProps } from 'vue';
-import { deletePost } from '@/api/posts';
-import { sweetalert, sweetconfirm } from '@/assets/common';
+import { ShareIcon, PencilIcon, TrashIcon } from "@heroicons/vue/24/solid";
+import { useRouter, useRoute } from "vue-router";
+import { defineProps } from "vue";
+import { deletePost } from "@/api/posts";
+import { sweetalert, sweetconfirm } from "@/assets/common";
 
 const router = useRouter();
 
@@ -64,51 +66,50 @@ const props = defineProps({
 const formatDate = (dateData) => {
   var thisData = dateData
     ?.substring(0, 16)
-    .replaceAll('-', '.')
-    .replace('T', ' ');
+    .replaceAll("-", ".")
+    .replace("T", " ");
   return thisData;
 };
 
 const goEdit = () => {
   router.push({
-    name: 'edit',
+    name: "edit",
     params: {
-      nav: 'wonseo',
+      nav: "wonseo",
       id: id,
     },
   });
 };
 
 const confirmDelete = () => {
-	sweetconfirm('진짜로 삭제할건가요??', 'warning').then(result => {
-		if (result.isConfirmed) doDelete();
-		else return;
-	});
+  sweetconfirm("진짜로 삭제할건가요??", "warning").then((result) => {
+    if (result.isConfirmed) doDelete();
+    else return;
+  });
 };
 
 const doDelete = async () => {
-	var response = await deletePost(id);
-	if (response.data.rowsAffected[0] >= 1) {
-		sweetalert('글이 삭제되었습니다!', 'success', function () {
-			router.push({ name: 'main', params: { nav: 'wonseo' } });
-		});
-	}
+  var response = await deletePost(id);
+  if (response.data.rowsAffected[0] >= 1) {
+    sweetalert("글이 삭제되었습니다!", "success", function () {
+      router.push({ name: "main", params: { nav: "wonseo" } });
+    });
+  }
 };
 </script>
 <style scoped>
 .DETAIL::-webkit-scrollbar {
-	width: 24px;
+  width: 24px;
 }
 ::-webkit-scrollbar {
   background-color: rgba(255, 255, 255, 0);
 }
 .DETAIL::-webkit-scrollbar-thumb {
-  border-left: 20px solid rgba(255, 255, 255, 0); 
+  border-left: 20px solid rgba(255, 255, 255, 0);
   background-clip: padding-box;
   background-color: hsl(var(--p));
 }
 .DETAIL::-webkit-scrollbar-track {
   background-color: rgba(255, 255, 255, 0);
 }
-
 </style>
