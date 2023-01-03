96675f747386908342cf4516eda58ae81328d54d
Young <navskh@naver.com>
2023-01-03 17:08:01 +0900

    Feat : snowflake

--- src/App.vue
+++ src/App.vue
@@ -15,4 +15,62 @@ import Sidebar from "./components/sidebar/Sidebar.vue";
 import Footer from "./components/Footer.vue";
 
 // var toTopButton = document.getElementById("topButton");
+
+var date = new Date();
+
+var month = date.getMonth() + 1;
+if ('12|1|2|'.indexOf(month) > -1) {
+  console.log('겨울이네요 눈이 내립니다! 헷');
+  doYouWannaBuildaSnowMan();
+}
+
+// 눈내리는 효과를 주기위해...
+function doYouWannaBuildaSnowMan() {
+  const body = document.querySelector('body');
+
+  const MIN_DURATION = 10;
+
+  function makeSnowFlake() {
+    const snowflake = document.createElement("div");
+    const delay = Math.random() * 10;
+    const initialOpacity = Math.random();
+    const duration = Math.random() * 20 + MIN_DURATION;
+
+    snowflake.classList.add('snowflake');
+    snowflake.style.left = `${Math.random() * window.screen.width}px`
+    snowflake.style.animationDelay = `${delay}s`;
+    snowflake.style.opacity = initialOpacity;
+    snowflake.style.animation = `fall ${duration}s linear`;
+
+    body.appendChild(snowflake);
+
+    setTimeout(() => {
+      body.removeChild(snowflake);
+      // makeSnowFlake();
+    }, (duration + delay) * 1000);
+  }
+
+  for (let index = 0; index < 50; index++) {
+      setTimeout(makeSnowFlake(), 500 * index);
+  }
+}
+
 </script>
+
+<style>
+.snowflake {
+  width: 8px;
+  height: 8px;
+  background-color: rgb(199, 199, 199);
+  border-radius: 50%;
+  position: absolute;
+  top: -8px;
+}
+@keyframes fall {
+    from{}
+    to{
+      transform: translateY(100vh);
+      opacity: 0;
+    }
+}
+</style>
